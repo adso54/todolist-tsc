2b14df8fbcabc0adcf054f7e4c33fdf5e5ce1800
kamil.danielski@gmail.com <kamil.danielski@gmail.com>
2021-03-29 19:33:53 +0200

    ToDo list in type script

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const todoItem_1 = require("./todoItem");
+const todoCollection_1 = require("./todoCollection");
+let todos = [
+    new todoItem_1.TodoItem(1, "Kupić kwiaty"),
+    new todoItem_1.TodoItem(2, "Odebrać buty"),
+    new todoItem_1.TodoItem(3, "Zamówić bilety"),
+    new todoItem_1.TodoItem(4, "Zadzwonić do Janka", true)
+];
+let collection = new todoCollection_1.TodoCollection("Adam", todos);
 console.clear();
-console.log("bla bla");
+console.log(`Właściciel listy: ${collection.userName}`
+    + `(liczba zadań pozostałych do zrobienia: ${collection.getItemCounts().incomplete})`);
+// let newId: number = collection.addTodo("Iść pobiegać");
+// let todoItem: TodoItem = collection.getTodoById(newId);
+// todoItem.printDetails();
+// collection.removeComplete();
+collection.getTodoItems(true).forEach(item => item.printDetails());
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,24 @@
+import { TodoItem } from "./todoItem";
+import { TodoCollection } from "./todoCollection";
+
+let todos: TodoItem[] = [
+    new TodoItem(1, "Kupić kwiaty"),
+    new TodoItem(2, "Odebrać buty"),
+    new TodoItem(3, "Zamówić bilety"),
+    new TodoItem(4, "Zadzwonić do Janka", true)
+];
+
+let collection: TodoCollection = new TodoCollection("Adam", todos);
+
 console.clear();
-console.log("bla bla");
+console.log(`Właściciel listy: ${collection.userName}` 
+    + `(liczba zadań pozostałych do zrobienia: ${collection.getItemCounts().incomplete})`
+    );
+
+// let newId: number = collection.addTodo("Iść pobiegać");
+// let todoItem: TodoItem = collection.getTodoById(newId);
+// todoItem.printDetails();
+
+// collection.removeComplete();
+
+collection.getTodoItems(true).forEach(item => item.printDetails());
